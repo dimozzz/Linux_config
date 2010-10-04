c4534f5f46eb4492ccfa1b994e465e65ae72320a
dimozzz <dimoz_88@rambler.ru>
2010-10-05 00:07:30 +0400

    1

--- home/dimozzz/.myScripts/cmplayer.sh
+++ home/dimozzz/.myScripts/cmplayer.sh
@@ -1,3 +1,20 @@
 #!/bin/zsh
 
-mplayer -vo fbdev -zoom -x "${2}" -y "${3}" "${1}"
+case $TERM in
+ linux|screen|screen.linux)
+	if [ "${#@}" = "3" ]; then
+		mplayer -vo fbdev -zoom -x "${2}" -y "${3}" "${1}"
+	fi
+	if [ "${#@}" = "2" ]; then
+		mplayer -vo fbdev -sub "${2}" -zoom -x "1024" -y "568" "${1}"
+	fi
+	if [ "${#@}" = "1" ]; then
+		mplayer -vo fbdev -zoom -x "1024" -y "568" "${1}"
+	fi
+ ;;
+
+
+ *xterm*|rxvt*|*terminal*|(dt|k|E)term)
+	echo "Run it in console"
+ ;;
+esac
