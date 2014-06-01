cfd4947d4dda6ad276bbc5881fb327f3723cdaa0
dimozzz <dimoz_88@rambler.ru>
2014-06-01 20:04:17 +0400

    1

--- home/dimozzz/.myScripts/check_mail.sh
+++ home/dimozzz/.myScripts/check_mail.sh
@@ -2,7 +2,9 @@
 gmail_login=""
 gmail_password=""
 fetch="$(wget --secure-protocol=TLSv1 --timeout=3 -t 1 -q -O - https://${gmail_login}:${gmail_password}@mail.google.com/mail/feed/atom --no-check-certificate )"
-line="$(echo "$fetch" | grep 'fullcount')"
+line1="$(echo "$fetch" | sed 's/<fullcount>/\n<fullcount>/')"
+line2="$(echo "$line1" | sed 's/<\/fullcount>/<\/fullcount>\n/')"
+line="$(echo "$line2" | grep 'fullcount')"
 unread="$(echo "$line" | sed "s/<fullcount>\(.*\)<\/fullcount>/\1/")"
 if [ -z "$unread" ]; then
 echo “!”
